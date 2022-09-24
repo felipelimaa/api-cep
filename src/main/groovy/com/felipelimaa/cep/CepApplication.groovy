fff5c4608909959ec205ddc78045f31c91b2e7b7
felipe <fearlimasi@gmail.com>
2022-09-23 21:55:21 -0300

    Initial commit

---
+++ src/main/groovy/com/felipelimaa/cep/CepApplication.groovy
@@ -0,0 +1,13 @@
+package com.felipelimaa.cep
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CepApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(CepApplication, args)
+	}
+
+}
